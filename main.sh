6ba18f3b727f844e67744c3b6365152e5446837f
cdchushig <cdavid.chushig@gmail.com>
2023-12-23 15:31:24 +0100

    Updated main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python src/main.py --type_data='unimodal' --type_modality='tabular'
-python src/main.py --type_data='unimodal' --type_modality='time_series'
-python src/main.py --type_data='unimodal' --type_modality='text'
-python src/main.py --type_data='multimodal' --type_fusion='early'
-python src/main.py --type_data='multimodal' --type_fusion='late'
+python src/train.py --type_data='unimodal' --type_modality='tabular'
+python src/train.py --type_data='unimodal' --type_modality='time_series'
+python src/train.py --type_data='unimodal' --type_modality='text'
+python src/train.py --type_data='multimodal' --type_fusion='early'
+python src/train.py --type_data='multimodal' --type_fusion='late'
 
 exit
